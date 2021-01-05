26a156da19709c58b2f7bed3163ff84dd272e0c8
haxnel <me@here.com>
2021-01-05 14:11:24 +0700

    First Commit ai

---
+++ main.py
@@ -0,0 +1,44 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def talk_command():
+    try:
+        with sr.Microphone() as source:
+            print('Listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'ella' in command:
+                command = command.replace('ella', '')
+                print(command)
+    except:
+        pass
+    return command
+
+def run_ella():
+    command = talk_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('Playing' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%H:%M')
+        print(time)
+        talk('Current time in your areas is '+ time)
+
+
+
+run_ella()
